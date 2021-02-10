eca368a7683277a9748eac2627b0be79349fccdb
tabasko <bucur37@gmail.com>
2021-02-10 15:50:56 +0200

    mobile view added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Coin from './Coin'
 
-function App() {
+
+const App = () => {
+
+  const [coins, setCoins] = useState<any[]>([])
+  const [search, setSearch] = useState('')
+
+ useEffect(() => {
+   axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+   .then(res => {
+     setCoins(res.data)
+   })
+   .catch(error => console.log("There is an " + error))
+ }, []);
+
+ const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+   setSearch(e.target.value)
+ }
+
+ const filteredCoins = coins.filter(coin => 
+  coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Crypto finder with CoinGecko API by Andrei Bucur @ <a href="https://codiver.ro">Codiver</a></h1>
+        <form>
+          <input type="text" className="coin-input" placeholder="Search" 
+          onChange={handleChange}/>
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin key={coin.id} 
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol}
+          marketcap={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
+          high={coin.high_24h}
+          low={coin.low_24h}/>
+      )})}
     </div>
   );
 }
---
+++ src/Coin.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Coin.css'
+
+const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap, high, low }: any) => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto"/>
+          <h1>{name}</h1>
+          <p className="coin-symbol">{symbol}</p>
+        </div>
+        <div className="coin-data">
+          <p className="coin-price">€ {price.toLocaleString()}</p>
+          <p className="coin-volume">{volume.toLocaleString()}</p>
+           {priceChange < 0 ? (
+             <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+           ) : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>)
+          }
+          <p className="coin-marketcap">
+            Market Cap: $ {marketcap.toLocaleString()}
+          </p>
+          <br></br>
+          <div className="coin-high-low">
+          <p>High 24h: €{high.toLocaleString()}</p>
+          <p>Low 24h: €{low.toLocaleString()}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Coin
